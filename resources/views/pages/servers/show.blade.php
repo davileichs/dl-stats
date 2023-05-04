41c8fec9707c57783a9ea0f6601f6b0524915c20
Davi L <davileichs@gmail.com>
2023-05-04 23:56:27 +0200

    move page

--- resources/views/pages/servers/show.blade.php
+++ resources/views/pages/servers/show.blade.php
@@ -16,6 +16,7 @@
                     <button class="nav-link link-light active" id="nav-status-tab" data-bs-toggle="tab" data-bs-target="#nav-status" type="button" role="tab" aria-controls="nav-status" aria-selected="true">Status</button>
                     <button class="nav-link link-light" id="nav-topplayers-tab" data-bs-toggle="tab" data-bs-target="#nav-topplayers" type="button" role="tab" aria-controls="nav-topplayers" aria-selected="false">Top Players</button>
                     <button class="nav-link link-light " id="nav-statistics-tab" data-bs-toggle="tab" data-bs-target="#nav-statistics" type="button" role="tab" aria-controls="nav-statistics" aria-selected="false">Statistics</button>
+                    <button class="nav-link link-light " id="nav-session-tab" data-bs-toggle="tab" data-bs-target="#nav-session" type="button" role="tab" aria-controls="nav-session" aria-selected="false">Sessions</button>
                 </div>
             </nav>
             <div class="tab-content pt-4">
@@ -23,10 +24,14 @@
                     @include('pages.servers.stats-players-tab')
                 </div>
                 <div class="tab-pane fade" id="nav-topplayers" role="tabpanel" aria-labelledby="nav-topplayers-tab">
-                    @include('pages.servers.top-players-tab')
+                    Desactived for a while because it demands too much memory for the poor server
+                    <!--@include('pages.servers.top-players-tab') -->
                 </div>
                 <div class="tab-pane fade" id="nav-statistics" role="tabpanel" aria-labelledby="nav-statistics-tab">
                     @include('pages.servers.statistics-server-tab')
+                </div>
+                <div class="tab-pane fade" id="nav-session" role="tabpanel" aria-labelledby="nav-session-tab">
+                    @include('pages.servers.sessions-map-tab')
                 </div>
             </div>
           </div>
